b6ab8612a2bdb3b36892872f6b34148461fcf9e3
Jovisx <jovisx14@gmail.com>
2020-05-31 15:01:23 -0500

    CorsHeders

--- TheTeam/settings.py
+++ TheTeam/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'TheTeam.urls'
