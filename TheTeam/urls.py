4d6ad6845c2b93d7cc5e8e05bffacb83cf3a30e9
Jovisx <jovisx14@gmail.com>
2020-05-31 01:57:00 -0500

    Creacion Urls Servicio

--- TheTeam/urls.py
+++ TheTeam/urls.py
@@ -20,11 +20,32 @@ from django.shortcuts import render
 
 from rest_framework.routers import DefaultRouter
 from Aplicaciones.encuesta.views import EncuestaViewSet
+from Aplicaciones.actividad.views import ActividadViewSet
+from Aplicaciones.empresa.views import EmpresaViewSet
+from Aplicaciones.rol.views import RolViewSet
+from Aplicaciones.usuario.views import UsuarioViewSet
 
 router = DefaultRouter()
 router.register(r'Aplicaciones.encuesta',EncuestaViewSet)
-
 urlpatterns = router.urls
+
+router = DefaultRouter()
+router.register(r'Aplicaciones.actividad',ActividadViewSet)
+urlpatterns += router.urls
+
+router = DefaultRouter()
+router.register(r'Aplicaciones.empresa',EmpresaViewSet)
+urlpatterns += router.urls
+
+router = DefaultRouter()
+router.register(r'Aplicaciones.rol',RolViewSet)
+urlpatterns += router.urls
+
+router = DefaultRouter()
+router.register(r'Aplicaciones.usuario',UsuarioViewSet)
+urlpatterns += router.urls
+
+
 urlpatterns += [
     path('admin/', admin.site.urls),
 ]
